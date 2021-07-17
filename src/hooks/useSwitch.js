bc1c7e77c259768c9bb828041fd39744613df955
Mohan Raj <mohanzolomon@gmail.com>
2021-07-17 21:06:23 +0530

    feat: add useSwitch hook

---
+++ src/hooks/useSwitch.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function useSwitch(initialState = false) {
+  const [state, setState] = useState(initialState);
+  const on = () => setState(true);
+  const off = () => setState(false);
+  const toggle = () => setState((value) => !value);
+  return { state, true: on, false: off, toggle };
+}
