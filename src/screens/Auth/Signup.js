dfa9a29fe735bcc65e9b6360451efe18cce77176
Mohan Raj <mohanzolomon@gmail.com>
2021-07-18 03:46:21 +0530

    feat: remove password autocomplete on signup screen

--- src/screens/Auth/Signup.js
+++ src/screens/Auth/Signup.js
@@ -62,7 +62,6 @@ export default function App() {
           value={password.value}
           onChangeText={password.set}
           autoCapitalize="none"
-          autoComplete="password"
           secureTextEntry={true}
         />
         <TextInput
