caee421f00a53ceb99828d821e9f20984ff9e124
Mohan Raj <mohanzolomon@gmail.com>
2021-07-18 02:14:16 +0530

    feat: add text input component

---
+++ src/components/TextInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, TextInput as Input } from 'react-native';
+
+// configs
+import colors from '../configs/colors';
+
+export default function TextInput(props) {
+  return (
+    <Input
+      value={props.value}
+      onChangeText={props.onChangeText}
+      style={styles.input}
+      placeholder={props.placeholder}
+      placeholderTextColor={colors.placeholder}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    width: 350,
+    height: 50,
+    borderRadius: 15,
+    borderWidth: 2,
+    borderColor: colors.primary,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    fontSize: 16,
+  },
+});
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 // components
 import Button from '../components/Button';
+import TextInput from '../components/TextInput';
 
 // configs
 import colors from '../configs/colors';
@@ -12,16 +13,8 @@ export default function App() {
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Quer</Text>
       <View style={styles.bottom}>
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          placeholderTextColor={colors.placeholder}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor={colors.placeholder}
-        />
+        <TextInput placeholder="Email" />
+        <TextInput placeholder="Password" />
         <Button text="Login" />
         <Text style={styles.newHere}>New Here? Signup</Text>
       </View>
@@ -46,16 +39,6 @@ const styles = StyleSheet.create({
     color: colors.primary,
     marginBottom: 300,
     textTransform: 'uppercase',
-  },
-  input: {
-    width: 350,
-    height: 50,
-    borderRadius: 15,
-    borderWidth: 2,
-    borderColor: colors.primary,
-    marginBottom: 20,
-    paddingHorizontal: 20,
-    fontSize: 16,
   },
   button: {
     backgroundColor: colors.primary,
