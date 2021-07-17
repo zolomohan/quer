475578751afc8ca2e9f034f45f0b365e9df8c383
Mohan Raj <mohanzolomon@gmail.com>
2021-07-17 20:18:35 +0530

    feat: add color config file

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Quer</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#a19ebb',
+  },
+});
---
+++ src/configs/colors.js
@@ -0,0 +1,22 @@
+const weights = {
+  w100: '#cfcddd',
+  w200: '#a19ebb',
+  w300: '#74719a',
+  w400: '#47477b',
+  w500: '#2a2e64',
+  w600: '#1e2456',
+  w700: '#121a49',
+  w800: '#05113c',
+  w900: '#000030',
+};
+
+const colors = {
+  white: '#fff',
+  black: '#000',
+  primary: weights.w900,
+  background: weights.w200,
+  placeholder: weights.w500,
+  weights: weights,
+};
+
+export default colors;
